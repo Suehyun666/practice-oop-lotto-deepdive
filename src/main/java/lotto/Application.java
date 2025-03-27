a3a487744e3ed1738ffed0668bc70fde82ab062d
Suehyun666 <em0200878@gmail.com>
2025-03-28 03:18:57 +0900

    refactor: AppConfig,GameController 도입

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,24 +1,12 @@
 package lotto;
 
-import lotto.service.GameManager;
-import lotto.util.generate.LottoGenerator;
-import lotto.util.generate.RandomLottoGenerator;
-import lotto.view.ConsoleInputView;
-import lotto.view.ConsoleOutputView;
-import lotto.view.InputView;
-import lotto.view.OutputView;
+import lotto.config.AppConfig;
+import lotto.controller.GameController;
 
 public class Application {
     public static void main(String[] args) {
-        InputView inputView = new ConsoleInputView();
-        OutputView outputView = new ConsoleOutputView();
-        LottoGenerator lottoGenerator = new RandomLottoGenerator();
-        GameManager gameManager = new GameManager(inputView, outputView, lottoGenerator);
-
-        try {
-            gameManager.startGame();
-        } catch (IllegalArgumentException e) {
-            System.out.println("[ERROR] " + e.getMessage());
-        }
+        AppConfig appConfig = new AppConfig();
+        GameController gameController = appConfig.gameController();
+        gameController.startGame();
     }
 }
