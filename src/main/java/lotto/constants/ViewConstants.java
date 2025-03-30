eec8213814f58c83c9575910f312356a100adf3b
Suehyun666 <em0200878@gmail.com>
2025-03-30 17:44:43 +0900

    refactor: View 상수 하나로 묶기

--- src/main/java/lotto/constants/ViewConstants.java
+++ src/main/java/lotto/constants/ViewConstants.java
@@ -1,7 +1,11 @@
 package lotto.constants;
 
-public class OutPutConstants {
-    public static final String OUTPUT_STATIC_START = "\n당첨 통계\n---";
+public class ViewConstants {
+    public static final String INPUT_BONUS = "\n보너스 번호를 입력해 주세요.";
+    public static final String INPUT_PRICE = "\n구입금액을 입력해 주세요. (100000원 이하의 1000원 단위)";
+    public static final String INPUT_WINNINGNUMBER = "\n당첨 번호를 입력해주세요. (1 ~ 45 숫자) \n 예시: 1,2,3,4,5,6 ";
+
+    public static final String OUTPUT_STATIC_START = "\"\\n당첨 통계\\n---\"";
 
     public static final String LOTTO_COUNT_MESSAGE2 = "개를 구매했습니다.";
     public static final String LOTTO_COUNT_MESSAGE1 = "\n";
